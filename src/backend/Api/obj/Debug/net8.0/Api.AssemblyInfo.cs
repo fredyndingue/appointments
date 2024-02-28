dc60d1314dfb78e3798c1293fa021b0c490cecd1
unknown <yannfredy.dev@gmail.com>
2024-02-28 16:49:37 +0100

    Created Api.Tests project

--- src/backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+518f2db821ee5c9be867177c9ac78f587510edef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a782b1d1f93d1077a5952ace340e7428b98081f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+518f2db821ee5c9be867177c9ac78f587510edef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a782b1d1f93d1077a5952ace340e7428b98081f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
