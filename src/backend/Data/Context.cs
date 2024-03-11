285f134ed633b299d8379508503e5df27992730f
unknown <yannfredy.dev@gmail.com>
2024-03-11 12:32:06 +0100

    Database context

---
+++ src/backend/Data/Context.cs
@@ -0,0 +1,23 @@
+﻿using Appointments.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apointments.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.Roles)
+                .HasConversion(
+                    u => string.Join(',', u),
+                    u => u.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
+        }
+    }
+}
