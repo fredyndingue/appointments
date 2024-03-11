ae2875f3b70603422d89a49f1ef4acfbd09f3259
unknown <yannfredy.dev@gmail.com>
2024-03-11 12:07:41 +0100

    Fix SignInUp models

---
+++ src/backend/Domain/Models/SignUpModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Appointments.Domain.Models
+{
+    public class SignUpModel
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        public List<string>? Roles { get; set; }
+    }
+}
